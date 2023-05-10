480301047bb4ba92a23e6deb1ffe19786461a11d
Sebastián Sala <sebastiansala.dev@gmail.com>
2023-05-10 20:57:00 -0300

    backend root page display woking message and links to swagger-ui docs

--- api/src/main/java/com/portfolio/api/controller/HealthCheckController.java
+++ api/src/main/java/com/portfolio/api/controller/HealthCheckController.java
@@ -1,22 +1,32 @@
 package com.portfolio.api.controller;
 
 import com.portfolio.api.dto.Message;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/")
 @CrossOrigin(origins = "*")
 public class HealthCheckController {
-  
+
   @GetMapping
-  public ResponseEntity checkHealth() {
-//    return new ResponseEntity(new Message("Backend working"), HttpStatus.OK);
-    return new ResponseEntity("Backend working OK", HttpStatus.OK);
+  public ResponseEntity<String> checkHealth(HttpServletRequest httpServletRequest) {
+
+//    return new ResponseEntity("Backend working OK, go to '/swagger-ui' for api documantation", HttpStatus.OK);
+    String swaggerUrl = ServletUriComponentsBuilder.fromRequest(httpServletRequest)
+        .replacePath("/swagger-ui")
+        .build().toUriString();
+
+    String message = String.format("Backend working OK, go to <a href=\"%s\">%s</a> for API documentation", swaggerUrl, swaggerUrl);
+
+    return new ResponseEntity<>(message, HttpStatus.OK);
+
   }
 
 }
