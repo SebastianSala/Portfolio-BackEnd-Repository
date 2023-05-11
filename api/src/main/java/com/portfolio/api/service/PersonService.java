1c263ff16feadab35b73e0573288ecc3a1e37a45
Sebastián Sala <sebastiansala.dev@gmail.com>
2023-05-11 20:53:52 -0300

    finetuning hikari for render deployment

--- api/src/main/java/com/portfolio/api/service/PersonService.java
+++ api/src/main/java/com/portfolio/api/service/PersonService.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@Transactional
+//@Transactional
 public class PersonService implements IPersonService {
 
   @Autowired
